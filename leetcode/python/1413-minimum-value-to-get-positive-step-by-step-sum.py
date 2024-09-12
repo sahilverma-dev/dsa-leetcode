abc1ad704ff18f8c1912d10d4c02d7b383cd6737
Sahil Verma <domainsahil007@gmail.com>
2024-09-12 15:21:43 +0530

    prefix sum 1413. Minimum Value to Get Positive Step by Step Sum

--- leetcode/python/1413-minimum-value-to-get-positive-step-by-step-sum.py
+++ leetcode/python/1413-minimum-value-to-get-positive-step-by-step-sum.py
@@ -3,22 +3,34 @@ from typing import List
 
 def minStartValue(nums: List[int]) -> int:
     # brute force
-    startValue = 1
+    # startValue = 1
 
-    while True:
-        curr_sum = startValue
-        valid = True
+    # while True:
+    #     curr_sum = startValue
+    #     valid = True
 
-        for num in nums:
-            curr_sum += num
-            if curr_sum <= 0:
-                valid = False
-                break
+    #     for num in nums:
+    #         curr_sum += num
+    #         if curr_sum <= 0:
+    #             valid = False
+    #             break
 
-        if valid:
-            return startValue
+    #     if valid:
+    #         return startValue
 
-        startValue += 1
+    #     startValue += 1
+
+    # prefix sum
+    min_prefix_sum = float("inf")
+    curr_prefix_sum = 0
+
+    for num in nums:
+        curr_prefix_sum += num
+
+        if curr_prefix_sum < min_prefix_sum:
+            min_prefix_sum = curr_prefix_sum
+
+    return max(1, -min_prefix_sum + 1)
 
 
 print("Example 1:", minStartValue([-3, 2, -3, 4, 2]))  # 5
